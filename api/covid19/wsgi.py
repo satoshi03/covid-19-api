71a265a24ec24f8e3eddb0d1c7ef84994d889dd3
satoshi03 <innamisatoshi@gmail.com>
2020-05-23 19:08:02 +0900

    fix to read dotenv file

--- api/covid19/wsgi.py
+++ api/covid19/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
+
+# if .env file exists on api dir, read it
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+if os.path.exists(dotenv_path):
+    dotenv.read_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covid19.settings')
 
